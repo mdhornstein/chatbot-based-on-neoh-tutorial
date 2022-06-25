55f219505f53aac8fd80305b209887c7804f112a
Michael Hornstein <mdhornstein@gmail.com>
2022-06-25 16:33:52 -0700

    Make a getMe API call

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import asyncio 
 import telegram 
 
+def display_user_info(user_object) :
+    print("****************************")
+    print(f"Info for user: {user_object.username}")
+    print("****************************")
+    print(f"is_bot: {user_object.is_bot}")
+    print(f"first_name: {user_object.first_name}")
+
+
+
+
 async def main(): 
-    bot = telegram.Bot("TOKEN")
+    # Load the API token 
+    with open("telegram_api_token.txt") as f:
+        api_token = f.readline().strip("\n")
+    print("**************************")
+    print(f"API token: {api_token}")
+    print("**************************")
+    bot = telegram.Bot(api_token)
+    
+    # Make a getMe API call. 
+    # https://core.telegram.org/bots/api#getme
+    # getMe: "A simple method for testing your bot's authentication token.
+    #         Returns basic information about the bot in the form of a 
+    #         User object."
     async with bot: 
-        print(await bot.get_me())
+        user_object = await bot.get_me()
+    
+    print(f"type(user_object): {type(user_object)}")
+    print(user_object)
+    print()
+    display_user_info(user_object)
 
 if __name__ == "__main__":
     asyncio.run(main())
