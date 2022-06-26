30d706ed14069601a71c1d0bbc2e17f43bc660a6
Michael Hornstein <mdhornstein@gmail.com>
2022-06-26 10:59:05 -0700

    Add echo_handler to echo non-command messages

--- first_bot_tutorial.py
+++ first_bot_tutorial.py
@@ -1,6 +1,6 @@
 import logging 
 from telegram import Update 
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", 
@@ -11,6 +11,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, 
                                    text="I'm a bot. Please talk to me!")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, 
+                                   text=update.message.text)
+
 if __name__ == "__main__":
     with open("telegram_api_token.txt") as f:
         api_token = f.readline().strip("\n")
@@ -18,5 +22,8 @@ if __name__ == "__main__":
 
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
 
     application.run_polling()
